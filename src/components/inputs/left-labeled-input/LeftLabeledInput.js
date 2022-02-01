3d2328cac41586296a5c488c000650e404c71843
ihsanaryandi <ihsan.webfront@gmail.com>
2022-02-01 13:20:05 +0700

    Creating Left Labeled Input

---
+++ src/components/inputs/left-labeled-input/LeftLabeledInput.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import appendClass from 'utils/class';
+import styles from './LeftLabeledInput.module.css'
+
+export default function LeftLabeledInput({ 
+    className = '', 
+    label, 
+    labelWidth = '15rem', 
+    type = 'text', 
+    placeholder = '', 
+    onChange
+}) {
+    return (
+        <div className={appendClass(styles['left-labeled-input'], className)}>
+            <label htmlFor={label} style={{ width: labelWidth }}>{label}</label>
+            <input type={type} id={label} placeholder={placeholder} onChange={onChange} />
+        </div>
+    );
+}
