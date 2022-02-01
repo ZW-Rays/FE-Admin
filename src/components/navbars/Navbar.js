b9a87a42659636b6ed02ce32f0bf14b236a70a5c
okiprianto_ <okistar123@gmail.com>
2022-02-01 12:10:47 +0700

    update navbar for drop down profile

--- src/components/navbars/Navbar.js
+++ src/components/navbars/Navbar.js
@@ -2,15 +2,24 @@ import React from 'react';
 import style from './Navbar.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
+import Container from 'components/containers/Container'
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
   <div className={style['navbar']}>
-
-    <h1 className="f-10">LOGO</h1>
-    <div className={style['navbar-profile']}>
-      <FontAwesomeIcon icon={faUser} size='4x'/>
+    <Container className={style['navbar-container']}>
+    <h1 className="f-6">LOGO</h1>
+    <div className={style['navbar-dropdown']}>
+      <button className={style['navbar-dropdown-btn']}>
+        <FontAwesomeIcon icon={faUser} size='2x'/>
+      </button>
+      <div className={style['navbar-dropdown-content']}>
+        <Link to={}>Detail Profile</Link>
+        <Link to={}>Log Out</Link>
+      </div>
     </div>
 
+    </Container>
   </div>);
 }
