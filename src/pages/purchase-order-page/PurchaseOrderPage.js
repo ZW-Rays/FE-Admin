e05ecbc9aafc081bc89bb5070187c1c7d77f4c34
okiprianto_ <okistar123@gmail.com>
2022-02-03 13:46:37 +0700

    create page list and create purchase order

--- src/pages/purchase-order-page/PurchaseOrderPage.js
+++ src/pages/purchase-order-page/PurchaseOrderPage.js
@@ -1,16 +1,9 @@
-import LabeledCard from 'components/cards/labeled-card/LabeledCard';
-import Container from 'components/containers/Container';
 import React from 'react';
-import LeftLabeledInput from 'components/inputs/left-labeled-input/LeftLabeledInput';
 
 export default function PurchaseOrderPage() {
   return (
-    <div className='w-full p-3'>
-        <LabeledCard label='Create Purchase Order' style={{ width: '100%' }}>
-            <LeftLabeledInput label='Purchase Order Number' style={{maxWidth : '500px'}}></LeftLabeledInput>
-            <LeftLabeledInput label='Purchase Order Date' type='date'></LeftLabeledInput>
-            <LeftLabeledInput label='Customer Name'></LeftLabeledInput>
-        </LabeledCard>
+    <div>
+
     </div>
   );
 }
